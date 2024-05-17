7a5a466c342e2cc4c3576c7c1488086ec6ddd441
Muhammad Salman <110420525+Salman1205@users.noreply.github.com>
2024-05-17 17:45:01 +0500

    Reducer.py

---
+++ Kafka_FIles/Reducer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict
+
+def reducer():
+    frequent_items_global = defaultdict(int)
+
+    # Read mapper output from standard input
+    for line in sys.stdin:
+        try:
+            item, count = line.strip().split('\t')
+            count = int(count)
+            # Combine local frequent itemsets to generate global frequent itemsets
+            frequent_items_global[item] += count
+        except ValueError:
+            # Skip lines with invalid format
+            continue
+
+    # Emit global frequent itemsets
+    for item, count in frequent_items_global.items():
+        print('%s\t%d' % (item, count))
+
+if __name__ == "__main__":
+    reducer()
